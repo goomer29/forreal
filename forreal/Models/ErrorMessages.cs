98974f7ab360c6d05ca929c47cd845933a065549
omergo hi <omergolan92@gmail.com>
2023-11-09 12:37:38 +0200

    made some proporties in Login

---
+++ forreal/Models/ErrorMessages.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forreal.Models
+{
+    public static class ErrorMessages
+    {
+        public const string INVALID_USERNAME = "Invalid username";
+
+        public const string INVALID_PASSWORD = "Invalid password";
+        public const string INVALID_LOGIN = "Invalid username or password";
+    }
+}
--- forreal/ViewModels/LoginPageViewModel.cs
+++ forreal/ViewModels/LoginPageViewModel.cs
@@ -4,6 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using forreal.Views;
+using forreal.Models;
+using System.Text.Json;
+
 
 namespace forreal.ViewModels
 {
@@ -16,22 +21,73 @@ namespace forreal.ViewModels
         private string _password;//סיסמה
 
         private bool _showPasswordError;//האם להציג שגיאת סיסמה
-        private string _passwordErrorMessage;
-        private bool _showLoginError;//
-        private string _loginErrorMessage;
+        private string _passwordErrorMessage;//תאור שגיאת סיסמה
+        private bool _showLoginError;//האם להציג שגיאת התחברות
+        private string _loginErrorMessage;//תאור שגיאת התחברות
         #endregion
         #region Service component
         private readonly ForrealService _service;
+        #endregion
+        #region Proporties
+        public string UserName
+        {
+            get => _userName;
+            set { if (_userName != value) { _userName = value; if (!ValidateUser()) { ShowUserNameError = true; UserErrorMessage = ErrorMessages.INVALID_USERNAME; } else { ShowUserNameError = true; UserErrorMessage = string.Empty; } OnPropertyChange(); OnPropertyChange(nameof(IsButtonEnabled)); } }
+        }    
+        public bool ShowUserNameError
+        {
+            get => _showUserNameError; set
+            {
+                if (_showUserNameError != value)
+                {
+                    _showUserNameError = value; OnPropertyChange();
+                }
+            }
+        }
+        public string UserErrorMessage { get => _userErrorMessage; 
+            set { if (_userErrorMessage != value) { _userErrorMessage = value; OnPropertyChange(); } } }
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                if (_password != value)
+                {
+                    _password = value; if (!ValidatePassWord())
+                    {
+                        ShowPasswordError = true;
+                        PasswordErrorMessage = ErrorMessages.INVALID_PASSWORD;
+                    }
+                    else
+                    {
+                        PasswordErrorMessage = string.Empty;
+                        ShowPasswordError = false;
+                    };
+                    OnPropertyChange();
+                    OnPropertyChange(nameof(IsButtonEnabled));
+                }
+            }
+        }
+        public bool ShowPasswordError
+        {
+            get => _showPasswordError; 
+            set { if (_showPasswordError != value) { _showPasswordError = value; OnPropertyChange(); } }
+        }
+        public string PasswordErrorMessage { get => _passwordErrorMessage; set { if (_passwordErrorMessage != value) { _passwordErrorMessage = value; OnPropertyChange(); } } }
+        public bool ShowLoginError { get => _showLoginError; set { if (_showLoginError != value) { _showLoginError = value; OnPropertyChange(); } } }
+        public string LoginErrorMessage { get => _loginErrorMessage; set { if (_loginErrorMessage != value) { _loginErrorMessage = value; OnPropertyChange(); } } }
+        //האם כפתור התחבר יהיה זמין
+        public bool IsButtonEnabled { get { return ValidatePage(); } }
         #endregion
         #region פעולות עזר
         private bool ValidateUser()
         {
             return !(string.IsNullOrEmpty(_userName) || _userName.Length < 3||_userName.Length>15);
         }
+
         private bool ValidatePassWord()
         {
-            //return !string.IsNullOrEmpty(Password);
-            return true;
+            return !string.IsNullOrEmpty(_password);
         }
 
         private bool ValidatePage()
