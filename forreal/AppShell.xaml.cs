1c6a5c80f67dd6dd3e6b672bd09c2fed59b2036e
User <User@LAB3-3>
2023-12-07 12:29:16 +0200

    appshell pog

--- forreal/AppShell.xaml.cs
+++ forreal/AppShell.xaml.cs
@@ -4,12 +4,17 @@ namespace forreal
 {
     public partial class AppShell : Shell
     {
+        private bool _isVisible  { get; set; }
         public AppShell()
         {
             InitializeComponent();
+            Routing.RegisterRoute("MainPage", typeof(MainPage));
             Routing.RegisterRoute("Login", typeof(LoginPage));
             Routing.RegisterRoute("SignUp", typeof(SignUpPage));
             Routing.RegisterRoute("HomePage", typeof(HomePage));
+            BindingContext = this;
+            IsVisible = false;
         }
+        public bool Visible{ get=>_isVisible; set { if(_isVisible!=value) _isVisible = value; OnPropertyChanged(); } }
     }
 }
